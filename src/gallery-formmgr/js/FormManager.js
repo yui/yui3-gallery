90ab595d38f4ef0ff22860441bd989300ae2581a
YUI Builder <build@yuilibrary.com>
2010-04-02 10:27:04 -0700

    gallery-2010.04.02-17-26 jafl gallery-formmgr

--- src/gallery-formmgr/js/FormManager.js
+++ src/gallery-formmgr/js/FormManager.js
@@ -1059,18 +1059,18 @@ FormManager.prototype =
 			{
 				this.displayMessage(e[i], info.error, 'error');
 				status = false;
-			}
-			if (!info.keepGoing)
-			{
 				continue;
 			}
 
-			if (this.validation.regex[e_id] &&
-				!this.validation.regex[e_id].test(e[i].value))
+			if (info.keepGoing)
 			{
-				this.displayMessage(e[i], msg_list ? msg_list.regex : null, 'error');
-				status = false;
-				continue;
+				if (this.validation.regex[e_id] &&
+					!this.validation.regex[e_id].test(e[i].value))
+				{
+					this.displayMessage(e[i], msg_list ? msg_list.regex : null, 'error');
+					status = false;
+					continue;
+				}
 			}
 
 			var f     = this.validation.fn[e_id];
