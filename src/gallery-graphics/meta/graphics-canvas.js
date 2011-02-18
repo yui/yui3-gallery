35c52fba53079425aa49c5662bea89eb918c18d2
YUI Builder <build@yuilibrary.com>
2011-02-18 15:10:55 -0800

    gallery-2011.02.18-23-10 tripp gallery-graphics

---
+++ src/gallery-graphics/meta/graphics-canvas.js
@@ -0,0 +1,4 @@
+function(Y) {
+    var canvas = document.createElement("canvas");
+    return (!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1") && (canvas && canvas.getContext && canvas.getContext("2d")));
+}
