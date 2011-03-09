03f1c5134dfd013cdae0e374947cada20b3af9b1
YUI Builder <build@yuilibrary.com>
2011-03-09 13:14:53 -0800

    gallery-2011.03.09-21-14 davglass gallery-oauth

--- src/gallery-oauth/js/oauth.js
+++ src/gallery-oauth/js/oauth.js
@@ -74,7 +74,7 @@
             }
          
             paramList = baseStr[2][0].split("&");
-            paramList.push("oauth_signature="+theSig);
+            paramList.push("oauth_signature=" + encodeURIComponent(theSig));
             paramList.sort(function(a,b) {
                 if (a[0] < b[0]) { return -1; }
                 if (a[0] > b[0]) { return 1; }
