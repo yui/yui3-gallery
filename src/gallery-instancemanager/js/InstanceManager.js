4de6cb50518fc09ff09dc4480b9a73a7190808c5
YUI Builder <build@yuilibrary.com>
2011-03-23 15:20:34 -0700

    gallery-2011.03.23-22-20 jafl gallery-instancemanager

--- src/gallery-instancemanager/js/InstanceManager.js
+++ src/gallery-instancemanager/js/InstanceManager.js
@@ -1,9 +1,9 @@
 "use strict";
 
 /**********************************************************************
- * <p>Stores instances of JavaScript components.  Allows a constructor to
- * be passed in place of an instance.  This enables lazy construction on
- * demand.</p>
+ * <p>Stores instances of JavaScript components.  Allows a constructor or
+ * factory method to be passed in place of an instance.  This enables lazy
+ * construction on demand.</p>
  * 
  * <p>One use is to create a global repository of JavaScript components
  * attached to DOM id's, e.g., YUI Buttons built on top of HTML
@@ -32,23 +32,10 @@ InstanceManager.prototype =
 	{
 		if (this._map[ id ] === null && this._constructors[ id ])
 		{
-			var c = this._constructors[ id ];
-
-			var s = 'new ' + (Y.Lang.isFunction(c.fn) ? 'c.fn' : c.fn) + '(';
-			if (c.args && c.args.length)
-			{
-				for (var i=0; i<c.args.length; i++)
-				{
-					if (i > 0)
-					{
-						s += ',';
-					}
-					s += 'c.args[' + i + ']';
-				}
-			}
-			s += ')';
-
-			this._map[ id ] = eval(s);
+			var c           = this._constructors[ id ];
+			var instance    = c.fn.prototype ? Y.Object(c.fn.prototype) : null;
+			var obj         = c.fn.apply(instance, c.args);
+			this._map[ id ] = Y.Lang.isUndefined(obj) ? instance : obj;
 		}
 
 		return this._map[ id ] || false;
@@ -69,7 +56,7 @@ InstanceManager.prototype =
 	 * Store an object or ctor+args.
 	 * 
 	 * @param id {String} The id of the object.
-	 * @param objOrCtor {Object|Function|String} The object or the object's constructor.
+	 * @param objOrCtor {Object|Function} The object or the object's constructor or a factory method.
 	 * @param args {Array} The array of arguments to pass to the constructor.
 	 * @return {boolean} false if the id has already been used
 	 */
@@ -82,8 +69,7 @@ InstanceManager.prototype =
 		{
 			return false;
 		}
-		else if (Y.Lang.isFunction(objOrCtor) ||
-				 Y.Lang.isString(objOrCtor))
+		else if (Y.Lang.isFunction(objOrCtor))
 		{
 			this._constructors[ id ] =
 			{
