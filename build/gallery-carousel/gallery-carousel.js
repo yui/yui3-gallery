bc28f079da6e0a58ea0cdcf6df13b46c5c7108e2
Gopal Venkatesan <g13n@ymail.com>
2011-05-18 23:04:48 +0530

    Fixed a bug that was passing NaN to setStyle
    IE is strict and better, Firefox is forgiving :-|

--- build/gallery-carousel/gallery-carousel.js
+++ build/gallery-carousel/gallery-carousel.js
@@ -605,6 +605,9 @@ Y.Carousel = Y.extend(Carousel, Y.Widget, {
             attr, cb, first, offset;
 
         index = self._getCorrectedIndex(index); // sanitize the value
+        if (isNaN(index)) {
+            return;
+        }
         offset = self._getOffsetForIndex(index);
         cb = self.get("contentBox");
         attr = self.get("isVertical") ? "top" : "left";
@@ -787,7 +790,7 @@ Y.Carousel = Y.extend(Carousel, Y.Widget, {
             numItems = self.get("numItems"),
             numVisible = self.get("numVisible"),
             sentinel = numItems - 1,
-            firstOfLastPage;
+            firstOfLastPage = 0;
 
         // Fix for Issues #2 and #11 - thanks <http://github.com/amasad>
         if (isCircular) {
@@ -1441,4 +1444,5 @@ Y.Carousel = Y.extend(Carousel, Y.Widget, {
 });
 
 
-}, '@VERSION@' ,{requires:['widget'], skinnable:true});
+
+}, '@VERSION@' ,{skinnable:true, requires:['widget']});
