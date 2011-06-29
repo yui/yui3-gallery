c2e1f5471ef10433baabe5bcc28189ccc131ab7a
YUI Builder <build@yuilibrary.com>
2011-06-29 13:08:52 -0700

    gallery-2011.06.29-20-12 jafl gallery-accordion-horiz-vert

--- src/gallery-accordion-horiz-vert/js/Accordion.js
+++ src/gallery-accordion-horiz-vert/js/Accordion.js
@@ -254,12 +254,12 @@ Accordion.HTML_PARSER =
 {
 	titles: function(content_box)
 	{
-		return content_box.all('li div:nth-child(1)');
+		return content_box.all('li > div:nth-child(1)');
 	},
 
 	sections: function(content_box)
 	{
-		return content_box.all('li div:nth-child(2)');
+		return content_box.all('li > div:nth-child(2)');
 	}
 };
 
@@ -413,6 +413,8 @@ Y.extend(Accordion, Y.Widget,
 		{
 			Y.log('ignoring titles & sections', 'info', 'Accordion');
 		}
+
+		this.get('contentBox').all('> li').remove();
 	},
 
 	/**
@@ -463,8 +465,16 @@ Y.extend(Accordion, Y.Widget,
 		if (el && this.get('replaceTitleContainer'))
 		{
 			var p = t.get('parentNode');
+			var n = t.get('nextSibling');
 			p.removeChild(t);
-			p.appendChild(el);
+			if (n)
+			{
+				p.insertBefore(el, n);
+			}
+			else
+			{
+				p.appendChild(el);
+			}
 
 			this.section_list[index].title = el;
 
@@ -524,8 +534,16 @@ Y.extend(Accordion, Y.Widget,
 			var display = d.getStyle('display');
 
 			var p = d.get('parentNode');
+			var n = d.get('nextSibling');
 			p.removeChild(d);
-			p.appendChild(el);
+			if (n)
+			{
+				p.insertBefore(el, n);
+			}
+			else
+			{
+				p.appendChild(el);
+			}
 
 			this.section_list[index].content = el;
 
@@ -768,7 +786,7 @@ Y.extend(Accordion, Y.Widget,
 
 	/**
 	 * @param {String|Node} any element inside the section or title
-	 * @return {int|null} the index of the containing section or <code>false</code> if not found
+	 * @return {int} the index of the containing section, or -1 if not found
 	 */
 	findSection: function(
 		/* string|element */	el)
@@ -787,7 +805,7 @@ Y.extend(Accordion, Y.Widget,
 			}
 		}
 
-		return false;
+		return -1;
 	},
 
 	/**
