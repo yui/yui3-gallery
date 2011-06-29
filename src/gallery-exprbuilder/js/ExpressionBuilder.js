203c241e4eab1b69358a8d6b259496d2d54720f5
YUI Builder <build@yuilibrary.com>
2011-06-29 13:08:22 -0700

    gallery-2011.06.29-20-12 jafl gallery-exprbuilder

--- src/gallery-exprbuilder/js/ExpressionBuilder.js
+++ src/gallery-exprbuilder/js/ExpressionBuilder.js
@@ -316,18 +316,18 @@ function clear(e)
 
 function insertQB(e)
 {
-	if (!this.qb_form.validateForm())
+	var qb = this.get('queryBuilder');
+	if (!qb.validateFields())
 	{
 		e.halt();
 		return;
 	}
 
-	var qb    = this.get('queryBuilder');
 	var query = qb.toDatabaseQuery();
 	if (query.length === 0)
 	{
 		var el = qb.get('contentBox').one('select');
-		this.qb_form.displayMessage(el, this.get('noVariableSelectedError'), 'error');
+		qb.displayFieldMessage(el, this.get('noVariableSelectedError'), 'error');
 		e.halt();
 		return;
 	}
@@ -372,9 +372,12 @@ function insertQB(e)
 
 function resetQB(e)
 {
-	this.qb_form.clearMessages();
 	this.get('queryBuilder').reset();
-	e.halt();
+
+	if (e)
+	{
+		e.halt();
+	}
 }
 
 function setValidation(f)
@@ -382,6 +385,14 @@ function setValidation(f)
 	if (f)
 	{
 		var self = this;
+
+		var orig_validateForm = f.validateForm;
+		f.validateForm = function()
+		{
+			resetQB.call(self);
+			orig_validateForm.apply(this, arguments);
+		};
+
 		f.setFunction(this.get('fieldId'), function(form, e)
 		{
 			return self._validateExpression(form, e, this);
@@ -409,8 +420,6 @@ Y.extend(ExpressionBuilder, Y.Widget,
 
 	renderUI: function()
 	{
-		var qb_form_name = Y.guid();
-
 		var container = this.get('contentBox');
 		container.set('innerHTML', this._field());
 
@@ -440,15 +449,12 @@ Y.extend(ExpressionBuilder, Y.Widget,
 		var qb = this.get('queryBuilder');
 		if (qb)
 		{
-			container.appendChild(Y.Node.create(this._query(qb_form_name)));
+			container.appendChild(Y.Node.create(this._query()));
 
 			qb.render(container.one('.'+this.getClassName('querybuilder')));
 
 			container.one('.'+this.getClassName('insert')).on('click', insertQB, this);
 			container.one('.'+this.getClassName('reset')).on('click', resetQB, this);
-
-			this.qb_form = new Y.FormManager(qb_form_name);
-			this.qb_form.prepareForm();
 		}
 	},
 
@@ -487,7 +493,8 @@ Y.extend(ExpressionBuilder, Y.Widget,
 		var qi    = -1;
 		for (var i=0; i<s.length; i++)
 		{
-			if (!quote && s[i] == '(')
+			var c = s.charAt(i);
+			if (!quote && c == '(')
 			{
 				if (paren === 0)
 				{
@@ -495,7 +502,7 @@ Y.extend(ExpressionBuilder, Y.Widget,
 				}
 				paren++;
 			}
-			else if (!quote && s[i] == ')')
+			else if (!quote && c == ')')
 			{
 				paren--;
 				if (paren < 0)
@@ -508,7 +515,7 @@ Y.extend(ExpressionBuilder, Y.Widget,
 					return false;
 				}
 			}
-			else if (s[i] == '\'' && (i === 0 || s[i-1] != '\\'))
+			else if (c == '\'' && (i === 0 || s.charAt(i-1) != '\\'))
 			{
 				if (!quote)
 				{
@@ -552,7 +559,7 @@ Y.extend(ExpressionBuilder, Y.Widget,
 	{
 		var markup =
 			'<div class="{td}">' +
-				'<textarea id="{tid}" name="{tn}" class="formmgr-field {ta}"></textarea>' +
+				'<textarea id="{tid}" name="{tn}" class="{ff} {ta}"></textarea>' +
 			'</div>' +
 			'<div class="{fctl}">' +
 				'<button class="{pc}">{paren}</button>' +
@@ -565,6 +572,7 @@ Y.extend(ExpressionBuilder, Y.Widget,
 		return Y.Lang.substitute(markup,
 		{
 			td:     this.getClassName('field-container'),
+			ff:     Y.FormManager.field_marker_class,
 			ta:     this.getClassName('field'),
 			tid:    this.get('fieldId'),
 			tn:     this.get('fieldName'),
@@ -584,25 +592,22 @@ Y.extend(ExpressionBuilder, Y.Widget,
 
 	/**
 	 * @protected
-	 * @return {String} markup for the QueryBuilder form
+	 * @return {String} markup for the QueryBuilder
 	 */
-	_query: function(
-		/* string */	qb_form_name)
+	_query: function()
 	{
 		var markup =
-			'<form name="{qbf}">' +
-				'<div class="{qb}"></div>' +
-				'<div class="{qbctl} formmgr-row">' +
-					'<button class="{ic}">{insert}</button>' +
-					'<button class="{rc}">{reset}</button>' +
-				'</div>' +
-			'</form>';
+			'<div class="{qb}"></div>' +
+			'<div class="{qbctl} {fr}">' +
+				'<button class="{ic}">{insert}</button>' +
+				'<button class="{rc}">{reset}</button>' +
+			'</div>';
 
 		return Y.Lang.substitute(markup,
 		{
-			qbf:	qb_form_name,
 			qb:     this.getClassName('querybuilder'),
 			qbctl:  this.getClassName('querybuilder-controls'),
+			fr:     Y.FormManager.row_marker_class,
 			ic:     this.getClassName('insert'),
 			rc:     this.getClassName('reset'),
 			insert: this.get('insertLabel'),
