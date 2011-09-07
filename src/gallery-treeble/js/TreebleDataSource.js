e5550cf27624ce5762e844bd496faa8c17ee28b9
YUI Builder <build@yuilibrary.com>
2011-09-07 13:35:30 -0700

    gallery-2011.09.07-20-35 jafl gallery-treeble

--- src/gallery-treeble/js/TreebleDataSource.js
+++ src/gallery-treeble/js/TreebleDataSource.js
@@ -711,7 +711,7 @@ function checkFinished()
 	}
 	else if (this._toggle.length > 0)
 	{
-		this.toggle(this._toggle[0], Y.clone(this._callback.request),
+		this.toggle(this._toggle[0], Y.clone(this._callback.request, true),
 		{
 			fn: function()
 			{
