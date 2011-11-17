9b56b7a258a3da631f43ecb260eb2071473cd503
YUI Builder <build@yuilibrary.com>
2011-11-17 06:47:38 -0800

    gallery-2011.11.17-14-47 solmsted gallery-composite-image

---
+++ src/gallery-composite-image/api/api.js
@@ -0,0 +1,3 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {"classes":["Composite.Image"],"modules":["gallery-composite-image"],"allModules":[{"displayName":"gallery-composite-image","name":"gallery-composite-image"}]} };
+});
