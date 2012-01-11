e69c9438fdcc449b31fcdad5ec4efbb1bff79b35
YUI Builder <build@yuilibrary.com>
2012-01-11 13:04:24 -0800

    gallery-2012.01.11-21-03 solmsted gallery-async-command-clone

---
+++ src/gallery-async-command-clone/api/api.js
@@ -0,0 +1,3 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {"classes":["Y.Async","Y.AsyncCommand","Y.Plugin.AsyncCommandClone"],"modules":["gallery-async","gallery-async-command","gallery-async-command-clone"],"allModules":[{"displayName":"gallery-async","name":"gallery-async"},{"displayName":"gallery-async-command","name":"gallery-async-command"},{"displayName":"gallery-async-command-clone","name":"gallery-async-command-clone"}]} };
+});
