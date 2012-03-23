41cd5d81354dc0a229317a7a9fcf9b4bdc243181
YUI Builder <build@yuilibrary.com>
2012-03-23 11:04:17 -0700

    gallery-2012.03.23-18-00 solmsted gallery-widget-inherit-css

---
+++ src/gallery-widget-inherit-css/api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "WidgetInheritCss"
+    ],
+    "modules": [
+        "gallery-widget-inherit-css"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-widget-inherit-css",
+            "name": "gallery-widget-inherit-css"
+        }
+    ]
+} };
+});
