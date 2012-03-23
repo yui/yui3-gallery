1e3b26dba716931ba897cabf3d356e9c747f132b
YUI Builder <build@yuilibrary.com>
2012-03-23 11:04:18 -0700

    gallery-2012.03.23-18-00 solmsted gallery-soon

---
+++ src/gallery-soon/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "YUI"
+    ],
+    "modules": [
+        "gallery-lazy-load"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-lazy-load",
+            "name": "gallery-lazy-load",
+            "description": "Similar to Y.later, but sooner.\n\nbased on setImmediate.js. https://github.com/NobleJS/setImmediate\n\nCopyright (c) 2011 Barnesandnoble.com, llc and Donavon West\n\nhttps://github.com/NobleJS/setImmediate/blob/master/MIT-LICENSE.txt"
+        }
+    ]
+} };
+});
