2de9d6369550173c3d4fcff43642932426b9523e
YUI Builder <build@yuilibrary.com>
2012-03-23 11:04:16 -0700

    gallery-2012.03.23-18-00 solmsted gallery-array-iterate

---
+++ src/gallery-array-iterate/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Array"
+    ],
+    "modules": [
+        "gallery-async-pause"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-async-pause",
+            "name": "gallery-async-pause",
+            "description": "Iterate through an array."
+        }
+    ]
+} };
+});
