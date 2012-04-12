4766635931c296498cdf9f73ae640a9654a5e356
YUI Builder <build@yuilibrary.com>
2012-04-12 06:51:30 -0700

    gallery-2012.04.12-13-50 solmsted gallery-cssmatrix2d

---
+++ src/gallery-cssmatrix2d/api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CSSMatrix2d"
+    ],
+    "modules": [
+        "gallery-cssmatrix2d"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-cssmatrix2d",
+            "name": "gallery-cssmatrix2d"
+        }
+    ]
+} };
+});
