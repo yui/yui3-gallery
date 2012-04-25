f76f3cb00b85312bfed87c436a7a4494ede7df1a
Zordius Chen <zordius@yahoo-inc.com>
2012-04-25 12:03:58 +0800

    add uninstalled method, add comments for yui doc, add more tests, add README

---
+++ src/gallery-zui-placeholder/README.md
@@ -0,0 +1,16 @@
+Welcome to ZUI placeholder
+==========================
+
+Introduction
+------------
+
+ZUI placeholder provides utilities to enable placeholder support for 
+older browsers.
+
+When user using browser which supports placeholder natively, ZUI placeholder
+just skip installation when utilities called. You do not need to care about
+user browser versions.
+
+Examples
+--------
+
--- src/gallery-zui-placeholder/js/zui-placeholder.js
+++ src/gallery-zui-placeholder/js/zui-placeholder.js
@@ -1,3 +1,9 @@
+/**
+ * The Placeholder module provides utilities to enable placeholder
+ * support for older browsers
+ *
+ * @module gallery-zui-placeholder
+ */ 
 var isNativeSupport = ('placeholder' in document.createElement('input')),
     txtPlaceHolderInstalled = 'data-phok',
     clsPlaceHolderBlur = 'zui-phblur',
@@ -25,8 +31,11 @@ var isNativeSupport = ('placeholder' in document.createElement('input')),
         }
     },
 
-    isInstalled = function (O) {
+    isInstalled = function (O, R) {
         if (O.getAttribute(txtPlaceHolderInstalled) === '1') {
+            if (R) {
+                O.setAttribute(txtPlaceHolderInstalled, '');
+            }
             return true;
         }
         O.setAttribute(txtPlaceHolderInstalled, '1');
@@ -55,13 +64,64 @@ var isNativeSupport = ('placeholder' in document.createElement('input')),
         }
 
         cntInstall += 1;
-    };
+    },
 
+    uninstallPH = function (O) {
+        if (!isInstalled(O, 1)) {
+            return;
+        }
+
+        // if no placeholder, stop
+        if (!O.getAttribute('placeholder')) {
+            return;
+        }
+
+        // remove focus, blur handler
+        O.detach('focus', handleFocus);
+        O.detach('blur', handleBlur);
+
+        handleFocus({currentTarget: O});
+        cntInstall += 1;
+    };
+/**
+ * A static object to access zui placeholder properties and methods
+ * @class Y.zui.placeholder
+ */
 Y.namespace('zui').placeholder = {
+    /**
+     * whether this browser supports placeholder natively
+     * @property isNative
+     * @static
+     * @type bool
+     */
     isNative: isNativeSupport,
+
+    /**
+     * A string used to set attribute to indicate this node installed placeholder or not
+     * @property txtInstalled
+     * @static
+     * @final
+     * @type string
+     */
     txtInstalled: txtPlaceHolderInstalled,
+
+
+    /**
+     * A string used to set classname when this input should show placeholder
+     * @property clsBlur
+     * @static
+     * @final
+     * @type string
+     */
     clsBlur: clsPlaceHolderBlur,
 
+    /**
+     * use this method to install placeholder on nodes
+     * @method install
+     * @param elements {NodeList || Node || HTMLElement || cssString} Optional. The elements to install placeholder support
+     * @return {Array} An array contains [TotalElements, InstalledElements] when no native placeholder support. return undefined when the browser suppports placeholder natively.
+     * @static
+     */
     install: isNativeSupport ? fNull : function (R) {
         var nodes = (R && R.each) ? R : Y.all(R || 'input, textarea');
 
@@ -71,6 +131,18 @@ Y.namespace('zui').placeholder = {
             return [0, 0];
         }
         nodes.each(installPH);
+
+        return [nodes.size(), cntInstall];
+    },
+
+    uninstall: isNativeSupport ? fNull : function (R) {
+        var nodes = (R && R.each) ? R : Y.all(R || 'input, textarea');
+
+        cntInstall = 0;
+        if (!nodes) {
+            return [0, 0];
+        }
+        nodes.each(uninstallPH);
 
         return [nodes.size(), cntInstall];
     },
