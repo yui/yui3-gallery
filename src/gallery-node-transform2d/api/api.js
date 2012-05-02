d50e5fdf0b6929f9a554938883ad686790afa508
YUI Builder <build@yuilibrary.com>
2012-05-02 13:11:14 -0700

    gallery-2012.05.02-20-10 solmsted gallery-node-transform2d

---
+++ src/gallery-node-transform2d/api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Node"
+    ],
+    "modules": [
+        "gallery-node-transform2d"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-node-transform2d",
+            "name": "gallery-node-transform2d"
+        }
+    ]
+} };
+});
