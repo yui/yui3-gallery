cb7117852f810ac923c2167db8d34525a3cf67a0
Zordius Chen <zordius@yahoo-inc.com>
2012-05-04 14:35:03 +0800

    use yuidocjs to generate document

---
+++ src/gallery-zui-placeholder/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Y.zui.placeholder"
+    ],
+    "modules": [
+        "gallery-zui-placeholder"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-zui-placeholder",
+            "name": "gallery-zui-placeholder",
+            "description": "The Placeholder module provides utilities to enable placeholder\nsupport for older browsers"
+        }
+    ]
+} };
+});
