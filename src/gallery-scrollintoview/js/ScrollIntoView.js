b69035d444dde35b8501e75215bc8673378ac2dc
YUI Builder <build@yuilibrary.com>
2012-05-16 13:39:20 -0700

    gallery-2012.05.16-20-37 jafl gallery-scrollintoview

--- src/gallery-scrollintoview/js/ScrollIntoView.js
+++ src/gallery-scrollintoview/js/ScrollIntoView.js
@@ -1,15 +1,24 @@
 "use strict";
 
-/**********************************************************************
+/**
+ * @module gallery-scrollintoview
+ */
+
+/**
  * <p>Only scrolls the browser if the object is not currently visible.</p>
  * 
  * <p>This requires that all scrollable elements have position:relative.
  * Otherwise, this algorithm will skip over them with unpredictable
  * results.</p>
  * 
- * @chainable
+ * @main gallery-scrollintoview
+ * @class Node~scrollIntoView
  */
 
+/**
+ * @method scrollIntoView
+ * @chainable
+ */
 Y.Node.prototype.scrollIntoView = function()
 {
 	var ancestor = Y.Node.getDOMNode(this.get('offsetParent'));
