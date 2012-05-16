f5398e3b12116f1cc8251367a0c08a45c2cb5d1f
YUI Builder <build@yuilibrary.com>
2012-05-16 13:39:07 -0700

    gallery-2012.05.16-20-37 jafl gallery-nodelist-extras2

--- src/gallery-nodelist-extras2/js/nodelist-extras2.js
+++ src/gallery-nodelist-extras2/js/nodelist-extras2.js
@@ -1,11 +1,15 @@
 "use strict";
 
-/**********************************************************************
+/**
+ * @module gallery-nodelist-extras2
+ */
+
+/**
  * <p>Augments Y.NodeList with the same higher-order functions that
  * array-extras adds to Y.Array.</p>
  * 
- * @module gallery-nodelist-extras2
- * @class NodeList
+ * @main gallery-nodelist-extras2
+ * @class NodeList~extras2
  */
 
 Y.mix(Y.NodeList.prototype,
@@ -16,6 +20,7 @@ Y.mix(Y.NodeList.prototype,
 	 * value.  The function receives the Node, the index, and the NodeList
 	 * itself as parameters (in that order).
 	 *
+	 * @method every
 	 * @param f {Function} the function to execute on each item
 	 * @param c {Object} optional context object
 	 * @return {Boolean} true if every item in the array returns true from the supplied function, false otherwise
@@ -36,6 +41,7 @@ Y.mix(Y.NodeList.prototype,
 	 * The function receives the Node, the index, and the NodeList itself
 	 * as parameters (in that order).
 	 *
+	 * @method find
 	 * @param f {Function} the function to execute on each item
 	 * @param c {Object} optional context object
 	 * @return {Node} the first Node for which the supplied function returns true, or null if it never returns true
@@ -57,6 +63,7 @@ Y.mix(Y.NodeList.prototype,
 	 * returns a new array with the results.  The function receives the
 	 * Node, the index, and the NodeList itself as parameters (in that order).
 	 *
+	 * @method map
 	 * @param f {String} the function to invoke
 	 * @param c {Object} optional context object
 	 * @return {Array} all return values, mapped according to the item key
@@ -78,6 +85,7 @@ Y.mix(Y.NodeList.prototype,
 	 * the Node, the index, and the NodeList itself as parameters (in that
 	 * order).
 	 *
+	 * @method partition
 	 * @param f {Function} the function to execute on each item
 	 * @param c {Object} optional context object
 	 * @return {Object} object with two properties: matches and rejects. Each is a NodeList containing the items that were selected or rejected by the test function (or an empty object if none).
@@ -109,6 +117,7 @@ Y.mix(Y.NodeList.prototype,
 	 * NodeList itself as parameters (in that order).  The function must
 	 * return the updated value.
 	 *
+	 * @method reduce
 	 * @param init {Mixed} the initial value
 	 * @param f {String} the function to invoke
 	 * @param c {Object} optional context object
