a8c4abaa919c229c5863ec50024af902f5fc9469
YUI Builder <build@yuilibrary.com>
2012-05-16 13:39:21 -0700

    gallery-2012.05.16-20-37 jafl gallery-chipper

--- src/gallery-chipper/js/Chipper.js
+++ src/gallery-chipper/js/Chipper.js
@@ -1,9 +1,13 @@
 "use strict";
 
+/**
+ * @module gallery-chipper
+ */
+
 /**********************************************************************
  * <p>Destroys objects asynchronously.</p>
  * 
- * @module gallery-chipper
+ * @main gallery-chipper
  * @class Chipper
  */
 
@@ -40,7 +44,7 @@ Y.Chipper =
 	 * Throw objects into the chipper.  If an object does not implement
 	 * destroy(), it is ignored.
 	 * 
-	 * @method Y.Chipper.destroy
+	 * @method destroy
 	 * @static
 	 * @param objs {Object/Array} The object(s) to destroy.
 	 */
