df02fd3684e1c4d38b9ec18de02537b87cd7eeb5
YUI Builder <build@yuilibrary.com>
2012-05-16 13:39:10 -0700

    gallery-2012.05.16-20-37 jafl gallery-expiration-cache

--- src/gallery-expiration-cache/js/ExpirationCache.js
+++ src/gallery-expiration-cache/js/ExpirationCache.js
@@ -1,25 +1,22 @@
-/**********************************************************************
+/**
+ * @module gallery-expiration-cache
+ */
+
+/**
  * <p>Cache which drops items based on a user-defined expiration criterion,
  * e.g., age.  By default, expired items are only removed when they are
  * requested.  If you want to "stop the world" and clean out the cache,
  * call clean().</p>
  * 
- * @module gallery-expiration-cache
+ * @main gallery-expiration-cache
  * @class ExpirationCache
  * @constructor
  * @param config {Object}
- *	<dl>
- *	<dt>store</dt>
- *	<dd>Data store which implements get,put,remove,clear,keys.  If not specified, a new instance of Y.InstanceManager is created.</dd>
- *	<dt>meta</dt>
- *	<dd>Function which attaches meta data to an item when it is added to the cache.  It receives the value as an argument.  If not specified, the default is to timestamp the item.</dd>
- *	<dt>expire</dt>
- *	<dd>(Required) Function which returns true if the item has expired.  It receives the meta data and the value as arguments.  If a number is specified, it is assumed to be a duration in milliseconds.</dd>
- *	<dt>stats</dt>
- *	<dd>Pass true if you want to collect basic statistics.  Pass a function if you want to control what information is stored for each key.  The function receives the key, the value, and the stat object.</dd>
- *	</dl>
+ * @param config.store {Object} Data store which implements get,put,remove,clear,keys.  If not specified, a new instance of `Y.InstanceManager` is created.
+ * @param config.meta {Function} Attaches meta data to an item when it is added to the cache.  It receives the value as an argument.  If not specified, the default is to timestamp the item.
+ * @param config.expire {Function} (Required) Returns true if the item has expired.  It receives the meta data and the value as arguments.  If a number is specified, it is assumed to be a duration in milliseconds.
+ * @param config.stats {Boolean} Pass true if you want to collect basic statistics.  Pass a function if you want to control what information is stored for each key.  The function receives the key, the value, and the stat object.
  */
-
 function ExpirationCache(config)
 {
 	this._store  = config.store || new Y.InstanceManager();
@@ -62,6 +59,7 @@ ExpirationCache.prototype =
 	/**
 	 * Retrieve a value.
 	 * 
+	 * @method get
 	 * @param key {String} the key of the object to retrieve
 	 * @return {Mixed} the stored object, or undefined if the slot is empty
 	 */
@@ -90,6 +88,7 @@ ExpirationCache.prototype =
 	/**
 	 * Store a value.
 	 * 
+	 * @method put
 	 * @param key {String} the key of the value
 	 * @param value {Object} the value to store
 	 * @return {boolean} false if the key has already been used
@@ -125,6 +124,7 @@ ExpirationCache.prototype =
 	/**
 	 * Store a value.
 	 * 
+	 * @method replace
 	 * @param key {String} the key of the value
 	 * @param value {Object} the value to store
 	 * @return {Mixed} the original value that was in the slot, or undefined if the slot is empty
@@ -141,6 +141,7 @@ ExpirationCache.prototype =
 	/**
 	 * Remove an value.
 	 * 
+	 * @method remove
 	 * @param key {String} the key of the value
 	 * @return {mixed} the value that was removed, or undefined if the slot was empty
 	 */
@@ -156,6 +157,8 @@ ExpirationCache.prototype =
 
 	/**
 	 * Remove all values.
+	 * 
+	 * @method clear
 	 */
 	clear: function()
 	{
@@ -164,6 +167,8 @@ ExpirationCache.prototype =
 
 	/**
 	 * Remove all expired values.
+	 * 
+	 * @method clean
 	 */
 	clean: function()
 	{
@@ -173,6 +178,7 @@ ExpirationCache.prototype =
 	/**
 	 * This resets all the values.
 	 *
+	 * @method dumpStats
 	 * @return {Object} the current stats
 	 */
 	dumpStats: function()
