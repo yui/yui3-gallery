cd111a9c256af664aa3e3effb710685857d8eb53
YUI Builder <build@yuilibrary.com>
2012-06-20 13:08:04 -0700

    gallery-2012.06.20-20-07 solmsted gallery-isoline

---
+++ src/gallery-isoline/api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Isoline"
+    ],
+    "modules": [
+        "gallery-isoline"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-isoline",
+            "name": "gallery-isoline"
+        }
+    ]
+} };
+});
