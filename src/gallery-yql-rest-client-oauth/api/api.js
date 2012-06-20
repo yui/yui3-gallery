10980113118ffd2b4342179f824417303874b987
YUI Builder <build@yuilibrary.com>
2012-06-20 13:08:30 -0700

    gallery-2012.06.20-20-07 solmsted gallery-yql-rest-client-oauth

---
+++ src/gallery-yql-rest-client-oauth/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "YQLRESTClient"
+    ],
+    "modules": [
+        "gallery-yql-rest-client-oauth"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-yql-rest-client-oauth",
+            "name": "gallery-yql-rest-client-oauth",
+            "description": "This module is a REST client supporting OAuth signatures."
+        }
+    ]
+} };
+});
