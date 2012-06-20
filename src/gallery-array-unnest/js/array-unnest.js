c73ca9183fa652a710b9d44a1d255445963ef2e5
YUI Builder <build@yuilibrary.com>
2012-06-20 13:08:34 -0700

    gallery-2012.06.20-20-07 solmsted gallery-array-unnest

--- src/gallery-array-unnest/js/array-unnest.js
+++ src/gallery-array-unnest/js/array-unnest.js
@@ -1,6 +1,22 @@
+/**
+ * Utility function that flattens nested arrays.
+ * @module gallery-array-unnest
+ */
 (function (Y) {
     'use strict';
     
+    /**
+     * Utility function that flattens nested arrays.
+     * The original array is not modified. The returned array items are a
+     * shallow copy of the original array items.
+     * @for Array
+     * @method unnest
+     * @param {Array} array
+     * @param {Number} levels Optional.  If defined, must be a non-negative
+     * integer.  Defaults to 1.
+     * @returns {Array}
+     * @static
+     */
     var unnest = function (array, levels) {
         var empty = [];
         
