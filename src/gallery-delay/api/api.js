b71a16c73396064422168c6c2fa362b63293a6b7
YUI Builder <build@yuilibrary.com>
2012-06-20 13:08:44 -0700

    gallery-2012.06.20-20-07 solmsted gallery-delay

---
+++ src/gallery-delay/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "YUI"
+    ],
+    "modules": [
+        "gallery-delay"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-delay",
+            "name": "gallery-delay",
+            "description": "Create a function that doesn't execute immediately when it is called."
+        }
+    ]
+} };
+});
