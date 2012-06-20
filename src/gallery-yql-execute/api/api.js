6830fabb9a5872e038cdebc197380830920b0ff0
YUI Builder <build@yuilibrary.com>
2012-06-20 13:08:29 -0700

    gallery-2012.06.20-20-07 solmsted gallery-yql-execute

---
+++ src/gallery-yql-execute/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "YQL"
+    ],
+    "modules": [
+        "gallery-yql-execute."
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-yql-execute.",
+            "name": "gallery-yql-execute.",
+            "description": "This module Executes JavaScript remotely."
+        }
+    ]
+} };
+});
