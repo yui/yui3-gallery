0965697474cfe8c27a55c53632ae0dc22ee5842f
YUI Builder <build@yuilibrary.com>
2012-07-11 14:39:33 -0700

    gallery-2012.07.11-21-38 solmsted gallery-conway

---
+++ src/gallery-conway/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Conway"
+    ],
+    "modules": [
+        "gallery-conway"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-conway",
+            "name": "gallery-conway",
+            "description": "A simulator for Conway's Game of Life."
+        }
+    ]
+} };
+});
