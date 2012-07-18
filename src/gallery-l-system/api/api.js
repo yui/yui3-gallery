84d07b94a10fed5bdfe78dcce790dcc04dfb63eb
YUI Builder <build@yuilibrary.com>
2012-07-18 06:23:41 -0700

    gallery-2012.07.18-13-22 solmsted gallery-l-system

---
+++ src/gallery-l-system/api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "LSystem"
+    ],
+    "modules": [
+        "gallery-l-system"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-l-system",
+            "name": "gallery-l-system"
+        }
+    ]
+} };
+});
