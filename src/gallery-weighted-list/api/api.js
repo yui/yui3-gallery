b319d4b27778326523761917387c0a8c54def2f9
YUI Builder <build@yuilibrary.com>
2012-07-18 06:23:42 -0700

    gallery-2012.07.18-13-22 solmsted gallery-weighted-list

---
+++ src/gallery-weighted-list/api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "WeightedList"
+    ],
+    "modules": [
+        "gallery-weighted-list"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-weighted-list",
+            "name": "gallery-weighted-list"
+        }
+    ]
+} };
+});
