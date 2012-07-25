54f94fa746e1c6b115647af7fa529a24aa7675aa
YUI Builder <build@yuilibrary.com>
2012-07-25 14:37:45 -0700

    gallery-2012.07.25-21-36 apipkin gallery-timer

---
+++ src/gallery-timer/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Y.Timer"
+    ],
+    "modules": [
+        "timer"
+    ],
+    "allModules": [
+        {
+            "displayName": "timer",
+            "name": "timer",
+            "description": "Losely modeled after AS3's Timer class. Provides a simple interface start,\n  pause, resume, and stop a defined timer set with a custom callback method."
+        }
+    ]
+} };
+});
