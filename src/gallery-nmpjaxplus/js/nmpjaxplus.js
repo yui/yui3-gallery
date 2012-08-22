88ea377f3a2505826ff58650ec5479326643f78d
YUI Builder <build@yuilibrary.com>
2012-08-22 13:01:20 -0700

    gallery-2012.08.22-20-00 joeauty gallery-nmpjaxplus

--- src/gallery-nmpjaxplus/js/nmpjaxplus.js
+++ src/gallery-nmpjaxplus/js/nmpjaxplus.js
@@ -70,6 +70,7 @@ Y.PjaxPlus = Y.Base.create('pjaxplus', Y.Widget, [], {
 						clickTarget:e.originEvent.target,
 						path:e.originEvent.target.get('pathname'),
 				    	url:e.originEvent.target.get('href'),
+						queryString:e.originEvent.target.get('search'),
 						html5support:html5support
 					}, this);
 				}
@@ -84,6 +85,7 @@ Y.PjaxPlus = Y.Base.create('pjaxplus', Y.Widget, [], {
 						clickTarget:clickedTarget,
 						path:clickedTarget.get('pathname'),
 				    	url:clickedTarget.get('href'),
+						queryString:clickedTarget.get('search'),							
 						html5support:html5support
 					}, this);
 				}
@@ -126,6 +128,7 @@ Y.PjaxPlus = Y.Base.create('pjaxplus', Y.Widget, [], {
 								clickTarget:e.target,
 								path:e.target.get('pathname'),
 						    	url:e.target.get('href'),
+								queryString:e.target.get('search'),
 								historyhash:historyhash,
 								html5support:html5support
 						    }, this);
@@ -169,6 +172,7 @@ Y.PjaxPlus = Y.Base.create('pjaxplus', Y.Widget, [], {
 								clickTarget:e.target,
 								path:e.target.get('pathname'),
 								url:e.target.get('href'),
+								queryString:e.target.get('search'),
 								historyhash:historyhash,
 								html5support:html5support
 							});
@@ -184,7 +188,12 @@ Y.PjaxPlus = Y.Base.create('pjaxplus', Y.Widget, [], {
 		if (this.get('startCallbackFunc') && !this.ppCache.retrieve(configObj.url)) {
 			//Y.log('trigger start callback - AJAX');
 			this.get('startCallbackFunc').call(null, configObj, this);
-		}	
+		}
+		if (!configObj.path.match(/^\//)) {
+			// make sure path has leading slash (IE seems to handle this differently)
+			configObj.path = "/" + configObj.path;
+		}
+		var loadpath = configObj.queryString ? configObj.path + configObj.queryString : configObj.path;
 			
 		var cfg = {
 			timeout: this.get('timeout'),
@@ -214,7 +223,7 @@ Y.PjaxPlus = Y.Base.create('pjaxplus', Y.Widget, [], {
 			}
 		};
 			
-		Y.io(configObj.url, cfg);
+		Y.io(loadpath, cfg);
 	}
 		        
 }, {
