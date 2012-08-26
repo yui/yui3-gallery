2849cfee6ec0f3138cd289bb0ef439123b646a61
stlsmiths <todd@stlsmiths.com>
2012-08-26 16:41:29 -0500

    initial commit

---
+++ src/gallery-datatable-selection/docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Y.DataTable.Selection"
+    ],
+    "modules": [
+        "DataTable",
+        "Selection"
+    ],
+    "allModules": [
+        {
+            "displayName": "DataTable",
+            "name": "DataTable"
+        },
+        {
+            "displayName": "Selection",
+            "name": "Selection",
+            "description": "A class extension for DataTable that adds \"highlight\" and \"select\" actions via mouse selection."
+        }
+    ]
+} };
+});
