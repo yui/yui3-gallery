b006ef4fc8df30e39374bc1ed80807c01a2772e9
YUI Builder <build@yuilibrary.com>
2012-09-19 13:07:46 -0700

    gallery-2012.09.19-20-07 stlsmiths gallery-calendar-jumpnav

---
+++ src/gallery-calendar-jumpnav/docs/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Y.Calendar.JumpNavView",
+        "Y.Plugin.Calendar.JumpNav"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
