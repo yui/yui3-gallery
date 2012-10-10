3bb7f4bb870b9d0b15f1f0eaa80b5e0d114f93bc
YUI Builder <build@yuilibrary.com>
2012-10-10 13:01:39 -0700

    gallery-2012.10.10-19-59 solmsted gallery-model-sync-multi

---
+++ src/gallery-model-sync-multi/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ModelSync.Multi"
+    ],
+    "modules": [
+        "gallery-model-sync-multi"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-model-sync-multi",
+            "name": "gallery-model-sync-multi",
+            "description": "Allows multiple model sync implementations to be used by a single model."
+        }
+    ]
+} };
+});
