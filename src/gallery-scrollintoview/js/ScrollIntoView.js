7740f88968a07a54e3e3c3d066659f6fc2c78d31
tripp <tripp.bridges@yahoo.com>
2013-04-24 14:57:48 -0700

    gallery-2013.04.24-22-00 jafl gallery-scrollintoview

--- src/gallery-scrollintoview/js/ScrollIntoView.js
+++ src/gallery-scrollintoview/js/ScrollIntoView.js
@@ -5,22 +5,209 @@
  */
 
 /**
- * <p>Only scrolls the browser if the object is not currently visible.</p>
+ * Only scrolls if the object is not currently visible.
  * 
- * <p>This requires that all scrollable elements have position:relative.
+ * This requires that all scrollable elements have position:relative.
  * Otherwise, this algorithm will skip over them with unpredictable
- * results.</p>
+ * results.
  * 
  * @main gallery-scrollintoview
  * @class Node~scrollIntoView
  */
 
+function scrollContainer(node)
+{
+	var info = node.scrollIntoViewData;
+	while (1)
+	{
+		var hit_top = (info.a.offsetParent === null);
+
+		var a = Y.one(info.a),
+			b = (Y.Node.getDOMNode(a) === Y.config.doc.body),
+			w = b ? Y.DOM.winWidth() : info.a.clientWidth,
+			h = b ? Y.DOM.winHeight() : info.a.clientHeight;
+		if (info.a.scrollWidth - a.horizMarginBorderPadding() > w ||
+			info.a.scrollHeight - a.vertMarginBorderPadding() > h)
+		{
+			break;
+		}
+		else if (hit_top)
+		{
+			node.fire('scrollIntoViewFinished');
+			node.scrollIntoViewData = null;
+			return false;
+		}
+
+		info.r.move(info.a.offsetLeft - info.a.scrollLeft, info.a.offsetTop - info.a.scrollTop);
+		info.a = info.a.offsetParent || info.a.parentNode;
+	}
+
+	var scrollX = (hit_top ? Y.config.doc.documentElement.scrollLeft || Y.config.doc.body.scrollLeft : info.a.scrollLeft);
+	var scrollY = (hit_top ? Y.config.doc.documentElement.scrollTop || Y.config.doc.body.scrollTop : info.a.scrollTop);
+
+	var d =
+	{
+		top:    scrollY,
+		bottom: scrollY + (hit_top ? Y.DOM.winHeight() : info.a.clientHeight),
+		left:   scrollX,
+		right:  scrollX + (hit_top ? Y.DOM.winWidth() : info.a.clientWidth)
+	};
+
+	if (hit_top && info.margin)
+	{
+		d.top    += info.margin.top    || 0;
+		d.bottom -= info.margin.bottom || 0;
+		d.left   += info.margin.left   || 0;
+		d.right  -= info.margin.right  || 0;
+	}
+
+	var dy = 0;
+	if (a.getStyle('overflowY') == 'hidden')
+	{
+		// don't scroll
+	}
+	else if (info.r.top < d.top)
+	{
+		dy = info.r.top - d.top;
+	}
+	else if (info.r.bottom > d.bottom)
+	{
+		dy = Math.min(info.r.bottom - d.bottom, info.r.top - d.top);
+	}
+
+	var dx = 0;
+	if (a.getStyle('overflowX') == 'hidden')
+	{
+		// don't scroll
+	}
+	else if (info.r.left < d.left)
+	{
+		dx = info.r.left - d.left;
+	}
+	else if (info.r.right > d.right)
+	{
+		dx = Math.min(info.r.right - d.right, info.r.left - d.left);
+	}
+
+	if (hit_top)
+	{
+		console.log(dx +',' + dy);
+		if (dx || dy)
+		{
+			if (info.anim)
+			{
+				var body = Y.one('body');
+				info.anim.setAttrs(
+				{
+					node: body,
+					to:
+					{
+						scrollLeft: body.get('scrollLeft') + dx,
+						scrollTop:  body.get('scrollTop') + dy
+					}
+				});
+
+				info.anim.once('end', function()
+				{
+					node.fire('scrollIntoViewFinished');
+				});
+				info.anim.run();
+			}
+			else
+			{
+				window.scrollBy(dx, dy);
+				node.fire('scrollIntoViewFinished');
+			}
+		}
+		else if (info.anim)
+		{
+			Y.later(0, null, function()
+			{
+				node.fire('scrollIntoViewFinished');
+			});
+		}
+
+		node.scrollIntoViewData = null;
+		return false;
+	}
+
+	console.log(dx +',' + dy);
+	if (dx || dy)
+	{
+		if (info.anim)
+		{
+			info.anim.setAttrs(
+			{
+				node: Y.one(info.a),
+				to:
+				{
+					scrollLeft: info.a.scrollLeft + dx,
+					scrollTop:  info.a.scrollTop + dy
+				}
+			});
+
+			info.anim.once('end', function()
+			{
+				info.r.move(info.a.offsetLeft - info.a.scrollLeft, info.a.offsetTop - info.a.scrollTop);
+				info.a = info.a.offsetParent;
+				scrollContainer(node);
+			});
+			info.anim.run();
+		}
+		else
+		{
+			info.a.scrollLeft += dx;
+			info.a.scrollTop  += dy;
+
+			info.r.move(info.a.offsetLeft - info.a.scrollLeft, info.a.offsetTop - info.a.scrollTop);
+			info.a = info.a.offsetParent;
+		}
+	}
+	else if (info.anim)
+	{
+		Y.later(0, null, function()
+		{
+			info.r.move(info.a.offsetLeft - info.a.scrollLeft, info.a.offsetTop - info.a.scrollTop);
+			info.a = info.a.offsetParent;
+			scrollContainer(node);
+		});
+	}
+	else
+	{
+		info.r.move(info.a.offsetLeft - info.a.scrollLeft, info.a.offsetTop - info.a.scrollTop);
+		info.a = info.a.offsetParent;
+	}
+
+	return true;
+}
+
 /**
+ * To receive notification of when the animation finishes, subscribe to the
+ * event `scrollIntoViewFinished` on the Y.Node instance.  For consistency,
+ * this event always fires, even without animation.
+ * 
+ * If you modify the ancestor chain while the animation is running, the
+ * results will be unpredictable.
+ *
  * @method scrollIntoView
+ * @param [config] configuration
+ * @param [config.anim] true to use default duration and easing, or object
+ * @param [config.anim.duration] duration of animation
+ * @param [config.anim.easing] easing used during animation
+ * @param [config.margin] viewport margins, to avoid ending up under an element with `position:fixed`
+ * @param [config.margin.top] top margin (px)
+ * @param [config.margin.bottom] bottom margin (px)
+ * @param [config.margin.left] left margin (px)
+ * @param [config.margin.right] right margin (px)
  * @chainable
  */
-Y.Node.prototype.scrollIntoView = function()
+Y.Node.prototype.scrollIntoView = function(config)
 {
+	if (this.scrollIntoViewData)
+	{
+		return this;
+	}
+
 	var ancestor = Y.Node.getDOMNode(this.get('offsetParent'));
 	if (!ancestor)
 	{
@@ -39,91 +226,38 @@ Y.Node.prototype.scrollIntoView = function()
 		/* int */	dx,
 		/* int */	dy)
 	{
+	console.log('move:'+dx+','+dy);
 		this.top    += dy;
 		this.bottom += dy;
 		this.left   += dx;
 		this.right  += dx;
 	};
 
-	while (1)
+	this.scrollIntoViewData =
 	{
-		while (1)
+		a: ancestor,
+		r: r
+	}
+
+	config                         = config || {};
+	this.scrollIntoViewData.margin = config.margin;
+
+	if (config.anim)
+	{
+		var anim_config = {};
+		if (Y.Lang.isObject(config.anim))
 		{
-			var hit_top = (ancestor.offsetParent === null);
-
-			var a = Y.one(ancestor),
-				b = (Y.Node.getDOMNode(a) === Y.config.doc.body),
-				w = b ? Y.DOM.winWidth() : ancestor.clientWidth,
-				h = b ? Y.DOM.winHeight() : ancestor.clientHeight;
-			if (ancestor.scrollWidth - a.horizMarginBorderPadding() > w ||
-				ancestor.scrollHeight - a.vertMarginBorderPadding() > h)
-			{
-				break;
-			}
-			else if (hit_top)
-			{
-				return this;
-			}
-
-			r.move(ancestor.offsetLeft - ancestor.scrollLeft, ancestor.offsetTop - ancestor.scrollTop);
-			ancestor = ancestor.offsetParent || ancestor.parentNode;
+			Y.mix(anim_config, config.anim);
 		}
+		this.scrollIntoViewData.anim = new Y.Anim(anim_config);
 
-		var scrollX = (hit_top ? Y.config.doc.documentElement.scrollLeft || Y.config.doc.body.scrollLeft : ancestor.scrollLeft);
-		var scrollY = (hit_top ? Y.config.doc.documentElement.scrollTop || Y.config.doc.body.scrollTop : ancestor.scrollTop);
-
-		var d =
+		scrollContainer(this);
+	}
+	else
+	{
+		while (scrollContainer(this))
 		{
-			top:    scrollY,
-			bottom: scrollY + (hit_top ? Y.DOM.winHeight() : ancestor.clientHeight),
-			left:   scrollX,
-			right:  scrollX + (hit_top ? Y.DOM.winWidth() : ancestor.clientWidth)
-		};
-
-		var dy = 0;
-		if (a.getStyle('overflowY') == 'hidden')
-		{
-			// don't scroll
-		}
-		else if (r.top < d.top)
-		{
-			dy = r.top - d.top;
-		}
-		else if (r.bottom > d.bottom)
-		{
-			dy = Math.min(r.bottom - d.bottom, r.top - d.top);
-		}
-
-		var dx = 0;
-		if (a.getStyle('overflowX') == 'hidden')
-		{
-			// don't scroll
-		}
-		else if (r.left < d.left)
-		{
-			dx = r.left - d.left;
-		}
-		else if (r.right > d.right)
-		{
-			dx = Math.min(r.right - d.right, r.left - d.left);
-		}
-
-		if (hit_top)
-		{
-			if (dx || dy)
-			{
-				window.scrollBy(dx, dy);
-			}
-			break;
-		}
-		else
-		{
-			ancestor.scrollLeft += dx;
-			ancestor.scrollTop  += dy;
-
-			r.move(ancestor.offsetLeft - ancestor.scrollLeft, ancestor.offsetTop - ancestor.scrollTop);
-
-			ancestor = ancestor.offsetParent;
+			// do it again
 		}
 	}
 
