2746fe3124efeaad0d72dc072589a8a1ff7fff7c
Reid Burke <me@reidburke.com>
2013-05-02 15:59:52 -0700

    gallery-2013.05.02-22-59 solmsted gallery-array-iterate

--- build/gallery-array-iterate/gallery-array-iterate.js
+++ build/gallery-array-iterate/gallery-array-iterate.js
@@ -1,39 +1,38 @@
 YUI.add('gallery-array-iterate', function (Y, NAME) {
 
 /**
- * Iterate through an array.
- * @module gallery-array-iterate
+Iterate through an array.
+@module gallery-array-iterate
+@author Steven Olmsted
  */
 (function (Y) {
     'use strict';
 
     /**
-     * @method iterate
-     * @for Array
-     * @param {Array} array The array to iterate
-     * @param {Number} [startIndex] The first index to iterate. If left
-     * undefined, iteration will either start at the beginning of the array
-     * if incrementBy is positive or at the end of the array if incrementBy
-     * is negative.
-     * @param {Number} incrementBy The interval by which the array will be
-     * iterated. Must be a non-zero integer. Negative values cause the array
-     * to be iterated backwards.
-     * @param {Function} iterationFunction The function to call on each
-     * iteration. This function will receive three arguments: value, index,
-     * and array. If this function returns a truthy value, iteration will be
-     * terminated.
-     * @param {Object} [contextObject] The context that will become this in
-     * the iterationFunction.
-     * @return {Boolean} Will return true if iteration was terminated early,
-     * otherwise it will return false.
-     * @static
-     */
-    var iterate = function (array, startIndex, incrementBy, iterationFunction, contextObject) {
+    @method iterate
+    @for Array
+    @param {Array} array The array to iterate
+    @param {Number} [startIndex] The first index to iterate. If left undefined,
+    iteration will either start at the beginning of the array if incrementBy is
+    positive or at the end of the array if incrementBy is negative.
+    @param {Number} incrementBy The interval by which the array will be
+    iterated.  Must be a non-zero integer.  Negative values cause the array to
+    be iterated backwards.
+    @param {Function} iterationFunction The function to call on each iteration.
+    This function will receive three arguments: value, index, and array.  If
+    this function returns a truthy value, iteration will be terminated.
+    @param {Object} [contextObject] The context that will become this in the
+    iterationFunction.
+    @return {Boolean} Will return true if iteration was terminated early,
+    otherwise it will return false.
+    @static
+    */
+    var _iterate = function (array, startIndex, incrementBy, iterationFunction, contextObject) {
         var i = startIndex,
             length = array.length;
         
         if (Y.Lang.isFunction(incrementBy)) {
-            return iterate(array, startIndex < 0 ? length - 1 : 0, startIndex, incrementBy, iterationFunction);
+            return _iterate(array, startIndex < 0 ? length - 1 : 0, startIndex, incrementBy, iterationFunction);
         }
 
         for (; i >= 0 && i < length; i += incrementBy) {
@@ -45,7 +44,7 @@ YUI.add('gallery-array-iterate', function (Y, NAME) {
         return false;
     };
 
-    Y.Array.iterate = iterate;
+    Y.Array.iterate = _iterate;
 }(Y));
 
-}, 'gallery-2012.12.05-21-01', {"requires": "yui-base"});
+}, 'gallery-2013.05.02-22-59', {"requires": "yui-base"});
